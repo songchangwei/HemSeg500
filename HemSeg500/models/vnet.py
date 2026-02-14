7356c0f260fe26f434f885ca82ffde47c4fa8762
songchangwei <1444675263@qq.com>
2026-02-14 18:16:19 +0800

    Delete HemSeg500/models directory

--- HemSeg500/models/vnet.py
+++
@@ -1,9 +0,0 @@
-import monai
-
-def vnet():
-        vnet = monai.networks.nets.VNet(
-                spatial_dims=3,
-                in_channels=1,
-                out_channels=1,
-        )
-        return vnet
